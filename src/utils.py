8b374172d63b7242e56a6bc54c036e4a5ff12b3a
Saketh <sakethry57@gmail.com>
2023-10-22 15:22:05 +0530

    hyperameters

--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,49 @@
 import os
 import sys
-import dill
 
-import numpy as np
+import numpy as np 
 import pandas as pd
+import dill
+import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
-        with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
-def evaluate_models(x_train,x_test,y_train,y_test,models):
+def evaluate_models(x_train,x_test,y_train,y_test,models,param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(x_train,y_train)
-            y_train_pred = model.predict(x_train)
-            y_test_pred = model.predict(x_test)
-            train_model_score = r2_score(y_train,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
-            report[list(models.keys())[i]] = test_model_score
+            para=param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
         return report
 
     except Exception as e:
